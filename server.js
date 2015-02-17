70b3a21ef662483c92dbd045581a240977875ad6
Yonatan <bityon@gmail.com>
2015-02-17 08:33:35 +0000

    Add upvotes, new style

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ router.use(express.static(path.resolve(__dirname, 'client')));
 
 //.use(express.static(__dirname + 'client'));
 
-server.listen(3000); 
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
